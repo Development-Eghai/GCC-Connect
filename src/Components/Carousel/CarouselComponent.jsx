1c5055f3e3b1b0cd27ab4bfe53755b3246e18cb6
DecoCrafttech <decocrafttech@gmail.com>
2025-01-27 12:31:48 +0530

    Updated: Home 1st sections corrections

--- src/Components/Carousel/CarouselComponent.jsx
+++ src/Components/Carousel/CarouselComponent.jsx
@@ -7,10 +7,10 @@ const CarouselComponent = () => {
         <Carousel interval={null} className='section-one'>
             <Carousel.Item>
                 <div className='carousel-item-cls'>
-                    <div className="col-9">
+                    <div className="col-12 col-md-8">
                         <p>Putting the right solutions and methods in place!</p>
-                        <h1 className='col-4 fw-bold'>Welcome to GCC Connect</h1>
-                        <p className='col-7'>GCC View360 product provides end to end solutions that are required by companies for setting up successful innovation centers in India. We will partner with companies at every stage of this process</p>
+                        <h1 className=' fw-bold'>Welcome to GCC Connect</h1>
+                        <p className=''>GCC View360 product provides end to end solutions that are required by companies for setting up successful innovation centers in India. We will partner with companies at every stage of this process</p>
                     <div className='d-flex col-12 justify-content-start'>
                     <div className="p-2 col-6 col-lg-3">
                     <button className="contact-btn w-100 ">Our Services</button>
